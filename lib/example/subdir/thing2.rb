5eed1aca2fcec25f0ffc178fff2aa659c011113a
Stuart Weir <cameron.weir@mytwu.ca>
2017-06-12 13:23:14 -0700

    rename method thing1 in thing2 to thing2

--- lib/example/subdir/thing2.rb
+++ lib/example/subdir/thing2.rb
@@ -7,7 +7,7 @@ module Example
         base.class_eval do
           class_option :statement, desc: 'Say something to name', default: 'Bye'
           desc 'thing2 NAME', 'do thing 2 with name'
-          def thing1(name)
+          def thing2(name)
             puts options.statement ? "#{options.statement}, #{name}" : name.to_s
           end
         end
